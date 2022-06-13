06b78fe1ce5d904374866903b84ab8044fdc9dd9
seminify <seminify@gmail.com>
2022-06-13 17:33:52 +0900

    template 적용

---
+++ src/main/java/kr/ac/jejunu/todo/controller/TodoController.java
@@ -0,0 +1,14 @@
+package kr.ac.jejunu.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TodoController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
