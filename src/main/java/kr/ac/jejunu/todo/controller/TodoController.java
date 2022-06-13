828c28f684c985a47f05afe6f0fe5bcf7708ea1f
seminify <seminify@gmail.com>
2022-06-13 17:42:56 +0900

    todo가 없을 때 숨기기

--- src/main/java/kr/ac/jejunu/todo/controller/TodoController.java
+++ src/main/java/kr/ac/jejunu/todo/controller/TodoController.java
@@ -1,14 +1,23 @@
 package kr.ac.jejunu.todo.controller;
 
+import kr.ac.jejunu.todo.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class TodoController {
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("todoCount", todoRepository.count());
         return "index";
     }
 }
