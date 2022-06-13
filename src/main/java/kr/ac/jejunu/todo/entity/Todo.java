e6432979b5b77c917c0f6e900c17c4acf858765b
seminify <seminify@gmail.com>
2022-06-13 17:36:22 +0900

    entity and repository 추가

---
+++ src/main/java/kr/ac/jejunu/todo/entity/Todo.java
@@ -0,0 +1,15 @@
+package kr.ac.jejunu.todo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+}
---
+++ src/main/java/kr/ac/jejunu/todo/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.jejunu.todo.repository;
+
+import kr.ac.jejunu.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
